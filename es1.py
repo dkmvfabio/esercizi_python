44b654a53a5e8299ca623e815e2a3fb87902a056
dkmvfabio <fabioclod67@outlook.it>
2024-11-11 19:46:41 +0100

    es1.py

--- es1.py
+++ es1.py
@@ -1,13 +1,14 @@
-datiMensili = (("Milano", [("Gennaio", 170),("Febbraio", 190),("Marzo", 120),("Aprile", 140),
-                          ("Maggio", 150),("Giugno", 90),("Luglio", "N/D"),("Agosto", "N/D"),
-                          ("Settembre", 220),("Ottobre", 200),("Novembre", 270),("Dicembre", 240)])
-               ("Bergamo", [("Gennaio", 230),("Febbraio", 170),("Marzo", 190),("Aprile", 170),
-                          ("Maggio", 210),("Giugno", 110),("Luglio", "N/D"),("Agosto", "N/D"),
-                          ("Settembre", "N/D"),("Ottobre", 130),("Novembre", 190),("Dicembre", 200)]),
-
-               ("Como", [("Gennaio", 110),("Febbraio", 150),("Marzo", 150),("Aprile", 190),
-                          ("Maggio", 130),("Giugno", "N/D"),("Luglio", "N/D"),("Agosto", "N/D"),
-                          ("Settembre", 190),("Ottobre", 100),("Novembre", 240),("Dicembre", "N/D")]))
+datiMensili = [
+    ("Milano", [("Gennaio", 170), ("Febbraio", 190), ("Marzo", 120), ("Aprile", 140),
+                ("Maggio", 150), ("Giugno", 90), ("Luglio", "N/D"), ("Agosto", "N/D"),
+                ("Settembre", 220), ("Ottobre", 200), ("Novembre", 270), ("Dicembre", 240)]),
+    ("Bergamo", [("Gennaio", 230), ("Febbraio", 170), ("Marzo", 190), ("Aprile", 170),
+                 ("Maggio", 210), ("Giugno", 110), ("Luglio", "N/D"), ("Agosto", "N/D"),
+                 ("Settembre", "N/D"), ("Ottobre", 130), ("Novembre", 190), ("Dicembre", 200)]),
+    ("Como", [("Gennaio", 110), ("Febbraio", 150), ("Marzo", 150), ("Aprile", 190),
+              ("Maggio", 130), ("Giugno", "N/D"), ("Luglio", "N/D"), ("Agosto", "N/D"),
+              ("Settembre", 190), ("Ottobre", 100), ("Novembre", 240), ("Dicembre", "N/D")])
+]
 
 nomeCitta = str(input("Inserisci il nome di una città da analizzare: "))
 
@@ -25,45 +26,32 @@ def analisiDati(nomeCitta, datiMensili):
     for citta, *dati in datiMensili:
         if(nomeCitta == citta):
             for mese, valore in dati:
-                if (valore == "N/D"):
-                    pass
-                else:
+                if (valore != "N/D"):
                     somma += valore
                     i+=1
-    media = somma/i
-
-    # Max
-    for citta, *dati in datiMensili:
-        if(nomeCitta == citta):
-            for mese, valore in dati:
-                if (valore == "N/D"):
-                    pass
-                else:
-                    if(valore>valoreMax):
-                        meseMax = mese
-                        valoreMax = valore
-
-    # Min
-    for citta, *dati in datiMensili:
-        if(nomeCitta == citta):
-            for mese, valore in dati:
-                if (valore == "N/D"):
-                    pass
-                else:
+                    # Min
                     if(valore<valoreMin):
                         meseMin = mese
                         valoreMin = valore
-    datiAnalizzati = (media,(valoreMax,meseMax),(valoreMin,meseMin))
+                    # Max
+                    if(valore>valoreMax):
+                        meseMax = mese
+                        valoreMax = valore
+    media = somma/i
+
+    return (media, (valoreMax, meseMax), (valoreMin, meseMin))
     
-    return print(datiAnalizzati)
-ciclo = True
-while ciclo:
+
+while True:
     nomeCitta = str(input("Inserisci il nome di una città da analizzare: "))
-    for citta, *dati in datiMensili:
-        if(nomeCitta==citta):
-            ciclo = False
+    ciclo = False
+    for citta, dati in datiMensili:
+        if(citta==nomeCitta):
+            ciclo = True
             break
-        else:
-            print("Nome città inserito non valido.")
+    if (ciclo==True):
+        break
+    else:
+        print("Nome città inserito non valido.")
 
 print(analisiDati(nomeCitta, datiMensili))
